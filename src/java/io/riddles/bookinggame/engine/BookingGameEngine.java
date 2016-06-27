a24aab78efb399762661dc3dbc19c443253e6948
Joost <joost@riddles.io>
2016-06-27 17:51:55 +0200

    Initial commit

---
+++ src/java/io/riddles/bookinggame/engine/BookingGameEngine.java
@@ -0,0 +1,33 @@
+package io.riddles.bookinggame.engine;
+
+import io.riddles.javainterface.engine.AbstractEngine;
+
+/**
+ * Created by joost on 6/27/16.
+ */
+public class BookingGameEngine extends AbstractEngine<BookingGameProcessor, BookingGamePlayer, BookingGameState> {
+    @Override
+    protected BookingGameState getInitialState() {
+        return null;
+    }
+
+    @Override
+    protected BookingGamePlayer createPlayer(int id) {
+        return null;
+    }
+
+    @Override
+    protected BookingGameProcessor createProcessor() {
+        return null;
+    }
+
+    @Override
+    protected void sendGameSettings(BookingGamePlayer player) {
+
+    }
+
+    @Override
+    protected String getPlayedGame(BookingGameState initialState) {
+        return null;
+    }
+}
